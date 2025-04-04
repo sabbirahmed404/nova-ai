2e6b6f41144a30c95eecad3aabc19220baa2dd32
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-04-04 18:52:06 +0600

    feat: Added home component and integrate inside main page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import HomePage from "@/components/home-page/home";
+
 export default function Home() {
-  return <div className=""></div>;
+  return <div className="">
+    <HomePage />
+  </div>;
 }
